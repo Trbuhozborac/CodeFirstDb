6eb315a1281418dda5759740e8424d8131336208
pomorange <nemanjapipi@hotmail.com>
2019-04-23 14:43:17 +0200

    prvi i poslednji komit

---
+++ CodeFirstDb/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace CodeFirstDb.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstDb.BloggingContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CodeFirstDb.BloggingContext";
+        }
+
+        protected override void Seed(CodeFirstDb.BloggingContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
